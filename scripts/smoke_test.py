b982140b406941081acd0321c943b7bef48650e1
ilyasZAOUIA <ilyaszaouiapro1@gmail.com>
2026-07-08 11:57:39 +0100

    add smoke

--- scripts/smoke_test.py
+++ scripts/smoke_test.py
@@ -3,8 +3,8 @@ import sys
 
 URL = sys.argv[1] if len(sys.argv) > 1 else "http://localhost:8069"
 DB = sys.argv[2] if len(sys.argv) > 2 else "demo"
-USER = "admin@gmail.com"
-PASSWORD = "admin_secure_pwd_2026"
+USER = "staging@gmail.com"
+PASSWORD = "staging"
 
 try:
     common = xmlrpc.client.ServerProxy(f'{URL}/xmlrpc/2/common')
