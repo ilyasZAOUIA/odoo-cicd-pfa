189c0e336899e5f9181eafe2f0a20043c8cd3b7d
ilyasZAOUIA <ilyaszaouiapro1@gmail.com>
2026-07-08 10:53:22 +0100

    Doc: stratégie migrations et rollback

--- custom_addons/mon_module_demo/models/materiel.py
+++ custom_addons/mon_module_demo/models/materiel.py
@@ -12,11 +12,15 @@ class MaterielConstruction(models.Model):
     fournisseur = fields.Char()
     date_reception = fields.Date()
     valeur_stock = fields.Float(compute='_compute_valeur_stock', store=True)
-    categorie = fields.Selection([
+
+    categorie = fields.Selection(
+        [
     ('gros_oeuvre', 'Gros œuvre'),
     ('second_oeuvre', 'Second œuvre'),
     ('finition', 'Finition'),
-    ], string='Catégorie', default='gros_oeuvre')
+        ],
+        string='Catégorie',
+        default='gros_oeuvre')
 
     @api.depends('quantite', 'prix_unitaire')
     def _compute_valeur_stock(self):
